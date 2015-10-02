83a4ea446e94740b3db4d5f2c9812143840349e1
bozana <bozana.bokan@fu-berlin.de>
2015-10-02 17:22:04 +0200

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file index.php
+ *
+ * Copyright (c) 2013-2015 Simon Fraser University Library
+ * Copyright (c) 2003-2015 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @package plugins.generic.anonymIP
+ *
+ * Wrapper for the plugin.
+ *
+ */
+
+require_once('AnonymIPPlugin.inc.php');
+return new AnonymIPPlugin();
+
+?>
